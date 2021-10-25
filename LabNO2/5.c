cc8c74007ef27c6c356d6ad317e512699da3be1a
BanTanger <88583317+BanTanger@users.noreply.github.com>
2021-10-25 23:39:37 +0800

    Add files via upload

--- LabNO2/5.c
+++ LabNO2/5.c
@@ -1,1 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int U, N, D, t = 0,tmp;
+//	scanf("%d%d%d", &N, &U, &D);
+//	tmp = 0;
+//	for (; tmp < N;)
+//	{
+//		t++;
+//		tmp += U;
+//		if (tmp >= N)break;
+//		t++;
+//		tmp -= D;
+//		if (tmp >= N)break;
+//	}
+//	printf("%d", t);
+//}
