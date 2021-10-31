0cce5c8b1e72bb49e4458fae36e3c72907206998
BanTanger <88583317+BanTanger@users.noreply.github.com>
2021-10-31 22:48:11 +0800

    Add files via upload

--- 10.30/3.c
+++ 10.30/3.c
@@ -7,19 +7,28 @@ int main()
 {
 	char ch;
 	char p = 0;
-	int count = 0;
-	char str[5] = {0,0,0,0,'\0'};
-	int i = 0;
+	//int count = 0;
+	//char str[5] = {0,0,0,0,'\0'};
+	//int i = 0;
+	int a = 0, b = 0, c = 0, d = 0;
 	while ((ch = getchar()) != '\n')
 		{
 			if (!isspace(ch))
 				p = ch;
-			if (p == 'D' || p == 'G' || p == 'U' || p == 'T')
-				str[i] = p;
-			i++;
+			if (p == 'D')
+				//str[i] = p;
+			//i++;
+				a = 1;
+			if (p == 'G')
+				b = 1;
+			if (p == 'U')
+				c = 1;
+			if (p == 'T')
+				d = 1;
 		}
 	//scanf("%s", &str);
-	if (strcmp(str,	"DGUT") == 0)
+	//if (strcmp(str,	"DGUT") == 0)
+	if (a + b + c + d == 4)
 		printf("能组成DGUT\n");
 	else
 		printf("检索的字母中不能组成DGUT\n");
