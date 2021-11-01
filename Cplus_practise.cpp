199086c123884ce4aa0a655f816c159866c8aacc
BanTanger <88583317+BanTanger@users.noreply.github.com>
2021-11-01 23:06:57 +0800

    Add files via upload

---
+++ Cplus_practise.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+#include<stdio.h>
+using namespace std;
+
+//int main()
+//{
+//	string str;
+//
+//	cout << "Please enter your name\n";
+//	getline(cin, str);
+//	cout << "Hello," << str << "!\n";
+//}
+
+//=======bool=======
+//int main()
+//{
+//	int a = 1, b = 1;
+//	bool b1 = a == b;
+//	cout << b1;
+//}
+
+//int main()
+//{
+//	char ch;
+//	for (int i = 1; i <= 5; i++)
+//	{
+//		ch = getchar();
+//		if (ch >= 'a' && ch <= 'u')
+//			ch += 5;
+//		else if (ch >= 'v' && ch <= 'z')
+//			ch = 'a' + ch - 'v';
+//		putchar(ch);
+//	}
+//}
+
+//int main()
+//{
+//	int a, b, c, d, f;
+//	cin >> a >> b;
+//	d = a, f = b;
+//	while (b){
+//		c = a % b;a = b;b = c;
+//	}
+//	cout << a << "\n";
+//	cout << f * d / a << "\n";
+//}
+
+//int main()
+//{
+//	int j = 1;
+//	int n;
+//	double A = 0.0;
+//	double Sn = 0.0;
+//	cin >> n;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		j *= i;
+//		A = 1.0 / j;
+//		Sn += A;
+//	}
+//	cout << Sn << "\n";
+//}
+
+//int main()
+//{
+//	char v[20] = "1d2w1dw0dawdwd";
+//	for (char* p = v; *p != 0; p++)
+//		cout << (*p) << ' ';
+//}
+
