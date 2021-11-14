a58960086dc31ed1202d8fba12cfa006eae8aac7
BanTanger <88583317+BanTanger@users.noreply.github.com>
2021-11-14 15:39:10 +0800

    Add files via upload

---
+++ C++练习笔记/初始化列表.cpp
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//using namespace std;
+//
+//class Person {
+//
+//public:
+//
+//	//传统初始化
+//	/*Person(int a, int b, int c) {
+//		m_A = a;
+//		m_B = b;
+//		m_C = c;
+//	}*/
+//
+//	//初始化列表
+//	Person(int a,int b ,int c) :m_A(a), m_B(b), m_C(c) {}
+//
+//	int m_A;
+//	int m_B;
+//	int m_C;
+//};
+//void test()
+//{
+//	//Person p(10, 20, 30);
+//	Person p;
+//	cout << "m_A = " << p.m_A << endl;
+//	cout << "m_B = " << p.m_B << endl;
+//	cout << "m_C = " << p.m_C << endl;
+//}
+//int main()
+//{
+//	test();
+//}
